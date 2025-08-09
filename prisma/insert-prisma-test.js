340553316677dfe02a19b7288cdf1eafac87b203
daviddinh1 <daviddinh602680@gmail.com>
2025-08-09 17:12:25 -0500

    committing all of the config files

---
+++ prisma/insert-prisma-test.js
@@ -0,0 +1,23 @@
+require("dotenv").config();
+const { PrismaClient } = require("../generated/prisma/index.js");
+
+const prisma = new PrismaClient();
+
+async function main() {
+  const testInsert = await prisma.test.create({
+    data: {
+      testComment: "second insert!!!!",
+    },
+  });
+
+  console.log("insert worked check terminal n supabase", testInsert);
+
+  const allRows = await prisma.test.findMany();
+  console.log("here is the results of the query", allRows);
+}
+
+main()
+  .catch(console.error)
+  .finally(async () => {
+    await prisma.$disconnect();
+  });
---
+++ prisma/migrations/20250809214351_first_prisma_migration/migration.sql
@@ -0,0 +1,7 @@
+-- CreateTable
+CREATE TABLE "public"."Test" (
+    "id" TEXT NOT NULL,
+    "testComment" TEXT,
+
+    CONSTRAINT "Test_pkey" PRIMARY KEY ("id")
+);
